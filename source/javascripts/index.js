db04ce8116d7ba11f91c8edaa5306ec139650f07
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-06-07 17:23:12 +0200

    testimonial slides

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,10 +1,30 @@
 $( document ).ready(function() {
-  // $('#typeform').hide();
-
   $('#go-to-typeform').click(function (){
-    // $('#typeform').hide();
     document.querySelector('#typeform').scrollIntoView({
       behavior: 'smooth'
     });
   });
+
+  $('.slide-next').click(function(){
+    var activeSlide = $('li.active');
+    activeSlide.removeClass('active').addClass('hidden');
+    var nextSlide = activeSlide.next();
+    if (nextSlide.length == 1) {
+      activeSlide.next().removeClass('hidden').addClass('active').show();
+    } else {
+      $('#slides li:first-child').removeClass('hidden').addClass('active').show();
+    }
+  });
+
+  $('.slide-previous').click(function(){
+    var activeSlide = $('li.active');
+    activeSlide.removeClass('active').addClass('hidden');
+    var previousSlide = activeSlide.prev();
+    if (previousSlide.length == 1) {
+      activeSlide.prev().removeClass('hidden').addClass('active').show();
+    } else {
+      $('#slides li:last-child').removeClass('hidden').addClass('active').show();
+    }
+  });
+
 });
