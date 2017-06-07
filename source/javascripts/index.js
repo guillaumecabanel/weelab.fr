e62b88199c2d1898ce389d05f1d62f4c7391cffe
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-06-07 10:45:59 +0200

    add testimonial style

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,5 +1,8 @@
 $( document ).ready(function() {
+  // $('#typeform').hide();
+
   $('#go-to-typeform').click(function (){
+    // $('#typeform').hide();
     document.querySelector('#typeform').scrollIntoView({
       behavior: 'smooth'
     });
