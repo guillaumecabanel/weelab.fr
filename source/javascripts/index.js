afd87b870835c724232668dba6428da360c45ccf
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-07-17 15:42:00 +0200

    redesign testimonials

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,14 +1,7 @@
 $( document ).ready(function() {
-  var qs,js,q,s,d=document;
-  var gi=d.getElementById, ce=d.createElement;
-  var gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/";
-  if(!gi.call(d,id)){
-    js=ce.call(d,"script");
-    js.id=id;
-    js.src=b+"embed.js";
-    q=gt.call(d,"script")[0];
-    q.parentNode.insertBefore(js,q)
-  }
+
+  // typeform js
+  (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
 
   $('.slide-next').click(function(){
     var activeSlide = $('li.active');
