6e32bb589a5429197da6b6004fc5da9a9d7b002c
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2017-07-24 18:34:36 +0200

    social links

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -3,12 +3,9 @@ $( document ).ready(function() {
   // typeform js
   (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
 
-  $(window).on('scroll', function(event){
-    if (event.originalEvent.pageY > 50){
-      $('header').slideDown();
-    };
-    if (event.originalEvent.pageY < 10){
-      $('header').slideUp();
-    };
-  });
+
+  // Social links animation
+  $('.social-link a').on('click', function(event){
+    $(this).parent().addClass('rotate-90');
+  })
 });
